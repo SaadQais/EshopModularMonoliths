f8db91db465d2db577eb05a9da3037cef4e0da94
Saad Qais <saadqais2013@live.com>
2025-07-11 15:39:47 +0300

    add DDD classes and interfaces to shared layer

---
+++ src/Shared/Shared/DDD/Aggregate.cs
@@ -0,0 +1,28 @@
+﻿namespace Shared.DDD
+{
+    public abstract class Aggregate<TId> : Entity<TId>, IAggregate<TId>
+        where TId : notnull
+    {
+        private readonly List<IDomainEvent> _domainEvents = [];
+
+        public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+        public IDomainEvent[] ClearDomainEvents()
+        {
+            var events = _domainEvents.ToArray();
+            _domainEvents.Clear();
+
+            return events;
+        }
+        
+        public void AddDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+        
+        public void RemoveDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Remove(domainEvent);
+        }
+    }
+}
---
+++ src/Shared/Shared/DDD/IDomainEvent.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Shared.DDD
+{
+    public interface IDomainEvent : INotification
+    {
+        Guid Id => Guid.NewGuid();
+        DateTime OccurredOn => DateTime.UtcNow;
+        string EventType => GetType().AssemblyQualifiedName ?? GetType().Name;
+    }
+}
