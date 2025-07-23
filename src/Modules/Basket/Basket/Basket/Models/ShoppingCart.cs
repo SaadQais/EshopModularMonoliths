3e22046ab871269f7fdb1db0e57755dbc6f528f4
Saad Qais <saadqais2013@live.com>
2025-07-23 16:33:37 +0300

    create basket domain model

---
+++ src/Modules/Basket/Basket/Basket/Models/ShoppingCart.cs
@@ -0,0 +1,51 @@
+﻿namespace Basket.Basket.Models
+{
+    public class ShoppingCart : Aggregate<Guid>
+    {
+        public string UserName { get; private set; } = string.Empty;
+        public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+
+        private readonly List<ShoppingCartItem> _items = [];
+        public IReadOnlyList<ShoppingCartItem> Items => _items.AsReadOnly();
+
+        public static ShoppingCart Create(string userName)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(userName);
+
+            return new ShoppingCart
+            {
+                Id = Guid.NewGuid(),
+                UserName = userName
+            };
+        }
+
+        public void AddItem(Guid productId, int quantity, string color, decimal price, string productName)
+        {
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quantity);
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+            var existingItem = _items.FirstOrDefault(x => x.ProductId == productId);
+
+            if (existingItem != null)
+            {
+                existingItem.Quantity += quantity;
+            }
+            else
+            {
+                var newItem = new ShoppingCartItem(Id, productId, quantity, color, price, productName);
+
+                _items.Add(newItem);
+            }
+        }
+
+        public void RemoveItem(Guid productId)
+        {
+            var existingItem = Items.FirstOrDefault(x => x.ProductId == productId);
+
+            if (existingItem != null)
+            {
+                _items.Remove(existingItem);
+            }
+        }
+    }
+}
